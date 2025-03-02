26c70bdd5d1b226151e3de4b9d6f409c5f3a639a
Vladimir <malyshewova@gmail.com>
2025-03-02 15:49:53 +0300

    - added style - plugged style

--- html/wp-content/themes/logomaker/header.php
+++ html/wp-content/themes/logomaker/header.php
@@ -27,36 +27,9 @@ $assets_path = $theme_path . '/assets'; ?>
 
 <div class="header-wrapper">
     <header class="header">
-        <div class="logo">
-            <a href="<?php echo home_url(); ?>">
-                <img class="logo_img-icon"src="<?php echo $assets_path; ?>/img/logoIcon.png" alt="Logo">
-                <img class="logo_img-text" src="<?php echo $assets_path; ?>/img/logoText.png" alt="CHICHA">
-            </a>
-        </div>
-        <div class="cart-container">
-            <a href="<?php echo site_url('cart'); ?>"><span class="cart-container__title ">КОРЗИНА</span>
-                <img src="<?php echo $assets_path; ?>/img/cartIcon.png" alt="Cart" class="cart-icon">
-                <div class="indicator"<?php if (empty($_SESSION['cart'])) : ?> style="display: none;" <?php endif; ?> id="indicator"></div>
-            </a>
-        </div>
+
     </header>
 
     <nav class="nav-bar">
-        <?php
-        $categories = get_categories();
-
-        usort($categories, function($a, $b) {
-            $order_a = get_field('custom_order', 'category_' . $a->term_id);
-            $order_b = get_field('custom_order', 'category_' . $b->term_id);
-            return $order_a - $order_b;
-        });
-
-        if (!empty($categories)) {
-            foreach ($categories as $category) { ?>
-                <button id="<?php echo esc_html($category->term_id); ?>"><?php echo esc_html($category->name); ?></button>
-            <?php }
-        } else {
-            echo '<p>Рубрики не найдены.</p>';
-        } ?>
     </nav>
 </div>
